80f8ac2dd371caf0cd47b3edd56a81e19b11a1b0
Gen1xLol <185639604+Gen1xLol@users.noreply.github.com>
2026-04-09 20:18:15 +0000

    Format code using Prettier

--- jest-setup.js
+++ jest-setup.js
@@ -1,13 +1,13 @@
-import fs from 'fs'
+import fs from "fs"
 
 console.log = (...args) => {
-  fs.writeFileSync(1, args.join(' ') + '\n')
+  fs.writeFileSync(1, args.join(" ") + "\n")
 }
 
 console.warn = (...args) => {
-  fs.writeFileSync(1, args.join(' ') + '\n')
+  fs.writeFileSync(1, args.join(" ") + "\n")
 }
 
 console.error = (...args) => {
-  fs.writeFileSync(2, args.join(' ') + '\n')
+  fs.writeFileSync(2, args.join(" ") + "\n")
 }
